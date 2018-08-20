b5cf8e341b51f7578ee11fd83cdaad5ce2dd1c72
merkelAlbert <merkel.albert1997@gmail.com>
2018-08-20 12:46:59 +0300

    Добавлены менеджеры для работы с пользователями

--- HRTool/Controllers/AccountController.cs
+++ HRTool/Controllers/AccountController.cs
@@ -1,13 +1,18 @@
-﻿using Microsoft.AspNetCore.Mvc;
+﻿using HRTool.Models;
+using Microsoft.AspNetCore.Identity;
+using Microsoft.AspNetCore.Mvc;
 
 namespace HRTool.Controllers
 {
     public class AccountController : Controller
     {
-        //здесь будет account
-        public IActionResult Index()
+        private readonly UserManager<User> _userManager;
+        private readonly SignInManager<User> _signInManager;
+
+        public AccountController(UserManager<User> userManager, SignInManager<User> signInManager)
         {
-            return null;
+            _userManager = userManager;
+            _signInManager = signInManager;
         }
     }
 }
