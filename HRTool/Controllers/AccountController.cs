159abe43aab030eec3232172d79c5f8b2bffd699
merkelAlbert <merkel.albert1997@gmail.com>
2018-08-20 12:42:24 +0300

    коммит в деволоп

--- HRTool/Controllers/AccountController.cs
+++ HRTool/Controllers/AccountController.cs
@@ -4,6 +4,7 @@ namespace HRTool.Controllers
 {
     public class AccountController : Controller
     {
+        //здесь будет account
         public IActionResult Index()
         {
             return null;
