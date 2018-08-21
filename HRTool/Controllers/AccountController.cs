0068fbc3f17638af5f2379a3a3f0aff1ccc6b074
Vyacheslav Lushin <locktarr123@gmail.com>
2018-08-21 09:14:48 +0300

    Добавлен метод Register

--- HRTool/Controllers/AccountController.cs
+++ HRTool/Controllers/AccountController.cs
@@ -14,5 +14,33 @@ namespace HRTool.Controllers
             _userManager = userManager;
             _signInManager = signInManager;
         }
+
+        [Route("Register/")]
+        public async Task<IActionResult> Register([FromQuery]User model)
+        {
+            if (ModelState.IsValid)
+            {
+                var existedUser = await _userManager.FindByEmailAsync(model.Email);
+                if (existedUser != null)
+                {
+                    ModelState.AddModelError("", "Аккаунт с данной электронной почтой уже зарегистрирован");
+                }
+                else
+                {
+                    //TODO Написать определение пользователя
+                    var user = new User {  };
+                    var result = await _userManager.CreateAsync(user, model.PasswordHash);
+
+                    if (result.Succeeded)
+                    {
+                        await _signInManager.SignInAsync(user, isPersistent: false);
+                        return new OkResult();
+                    }
+                }
+                
+            }   
+
+            return new OkResult();
+        }
     }
 }
