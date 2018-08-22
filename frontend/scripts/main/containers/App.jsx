2254fcb03356916b725be721225749df816b09f7
Artemov Anton <anton.artemov2011@yandex.ru>
2018-08-22 09:07:05 +0300

    Доработан дизайн

--- frontend/scripts/main/containers/App.jsx
+++ frontend/scripts/main/containers/App.jsx
@@ -1,5 +1,6 @@
 import React from 'react';
 import { Redirect, Switch, Route } from 'components/router';
+import UserProvider from 'containers/UserProvider';
 
 import Layout from './Layout';
 import AuthForm from '../components/AuthForm';
@@ -7,14 +8,16 @@ import RegForm from '../components/RegForm';
 import NotFound from '../components/NotFound';
 
 const App = () => (
-  <Layout>
-    <Switch>
-      <Redirect exact from="/" to="/home" />
-      <Route path="/autorisation" component={AuthForm} />
-      <Route path="/registration" component={RegForm} />
-      <Route path="*" component={NotFound} />
-    </Switch>
-  </Layout>
+  <UserProvider>
+    <Layout>
+      <Switch>
+        <Redirect exact from="/" to="/home" />
+        <Route path="/autorisation" component={AuthForm} />
+        <Route path="/registration" component={RegForm} />
+        <Route path="*" component={NotFound} />
+      </Switch>
+    </Layout>
+  </UserProvider>
 );
 
 export default App;
--- frontend/scripts/main/containers/Layout.jsx
+++ frontend/scripts/main/containers/Layout.jsx
@@ -1,14 +1,43 @@
-import React from 'react';
+import React, { Component } from 'react';
 import PropTypes from 'prop-types';
+import connect from 'decorators/ConnectDecorators';
+import { Link } from 'components/router';
 
-const Layout = ({ children }) => (
-  <div className="layout">
-    {children}
-  </div>
-);
+import UserInfo from '../components/UserInfo';
+import logo from '../../../images/logo.png';
+
+class Layout extends Component {
+  render() {
+    const { info, children } = this.props;
+
+    return (
+      <div className="layout">
+        <div className="header">
+          <Link to="/">
+            <img src={logo} alt="" className="logo" />
+          </Link>
+          <ul className="nav-links">
+            <li className="nav-item">
+              <Link className="nav-link" to="/vacancies">Вакансии</Link>
+            </li>
+            <li className="nav-item">
+              <Link className="nav-link" to="/resumes">Резюме</Link>
+            </li>
+          </ul>
+          <UserInfo user={info} />
+        </div>
+        {children}
+        <div className="footer">
+          <div className="copyright">© 2018, ЗАО «Калуга Астрал»</div>
+        </div>
+      </div>
+    );
+  }
+}
 
 Layout.propTypes = {
-  children: PropTypes.node
+  info: PropTypes.object,
+  children: PropTypes.node,
 };
 
-export default Layout;
+export default connect({ name: 'user' })(Layout);
