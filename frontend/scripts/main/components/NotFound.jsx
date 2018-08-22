a46e80cd7ab2a569d9ed17cbb6e09c7757bc2fc3
Artemov Anton <anton.artemov2011@yandex.ru>
2018-08-22 10:07:01 +0300

    Доработана страница 404

--- frontend/scripts/main/components/NotFound.jsx
+++ frontend/scripts/main/components/NotFound.jsx
@@ -1,11 +1,13 @@
 import React from 'react';
 import { Link } from 'components/router';
 
+import notFound from '../../../images/404.png';
+
 const MainLinksNotFound = () => (
   <div className="not-found">
-    <h2 className="not-found__h2">К сожалению мы не смогли найти запрашиваемую страницу</h2>
-    <video autoPlay loop src="https://media.giphy.com/media/xTiN0L7EW5trfOvEk0/giphy.mp4"></video>
-    <p className="not-found__p">Попробуйте начать с <Link to="/">главной страницы</Link></p>
+    <h2>Страница не найдена</h2>
+    <img src={notFound} />
+    <Link to="/">На главную страницу</Link>
   </div>
 );
 
