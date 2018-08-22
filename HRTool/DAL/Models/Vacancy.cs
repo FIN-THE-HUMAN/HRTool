8fb7a3dd69552812d856fb1cd6c84a51dce1e862
m1raz3nt <m1raz3nt@gmail.com>
2018-08-22 11:58:30 +0200

    Изменения в моделях БД
    Изменения касаются, в первую очередь, модели вакансий

--- HRTool/DAL/Models/Vacancy.cs
+++ HRTool/DAL/Models/Vacancy.cs
@@ -1,6 +1,8 @@
 using System;
 using Microsoft.AspNetCore.Identity;
 using Microsoft.AspNetCore.Mvc;
+using System.ComponentModel.DataAnnotations;
+using HRTool.DAL.Models;
 
 namespace HRTool.Models
 {
@@ -9,10 +11,26 @@ namespace HRTool.Models
     {
         public Guid Id {get;set;}
         public string Name {get; set;}
-        public decimal Salary {get; set;}
+        //"Заработок: От 35000р...
+        public decimal SalaryRangeFrom {get; set;}
+        //...до 200000р"
+        public decimal SalaryRangeTo {get; set;}
+        //Требуемый опыт работы
+        public string RequiredExperienceRange {get;set;}
+        //Имя и фамилия контактного лица
         public string ContactPerson {get;set;}
-        public string Phone {get;set;}
+        [Phone]
+        public string ContactPhone {get;set;}
+        [EmailAddress]
+        public string ContactMail {get;set;}
+        //Полная, неполная занятость
         public string EmploymentType {get; set;}
+        //Часы работы в день
+        public string WorkHours {get;set;}
         public string Description {get; set;}
+        //Чем будет заниматься соискатель(обязанности)
+        public DutyDictionary Duties {get;set;}
+        //Что должен уметь соискатель (требования)
+        public RequirementsDictionary Requirements {get;set;}
     }
 }
