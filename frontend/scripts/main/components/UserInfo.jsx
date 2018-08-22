43f9109ed4542dce9d79536ae02eb0d16a1c139b
Artemov Anton <anton.artemov2011@yandex.ru>
2018-08-22 09:06:27 +0300

    Добавлена информация о пользователе

---
+++ frontend/scripts/main/components/UserInfo.jsx
@@ -0,0 +1,44 @@
+import React from 'react';
+import PropTypes from 'prop-types';
+import SessionService from 'services/SessionService';
+import WindowService from 'services/WindowService';
+import { DropdownButton, MenuItem } from 'react-bootstrap';
+
+import avatar from '../../../images/avatar.svg';
+
+const UserInfo = () => {
+  const onLogout = () => {
+    SessionService.signOut();
+    WindowService.redirect(WindowService.location);
+  };
+
+  return (
+    <div className="user-info">
+      <DropdownButton
+        pullRight
+        bsStyle="link"
+        title={
+          <span>
+            <img className="avatar" src={avatar} />
+            User
+          </span>
+        }
+        id="user-info"
+      >
+        <MenuItem header>User</MenuItem>
+        <MenuItem eventKey="2">Профиль</MenuItem>
+        <MenuItem eventKey="3">Настройки</MenuItem>
+        <MenuItem divider />
+        <MenuItem eventKey="4" onClick={onLogout}>Выйти</MenuItem>
+      </DropdownButton>
+    </div>
+  );
+};
+
+UserInfo.propTypes = {
+  user: PropTypes.object
+};
+
+export default UserInfo;
+
+
