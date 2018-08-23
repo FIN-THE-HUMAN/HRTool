d795e6f2bf1d5d9f6f4716e0428625153cab316b
m1raz3nt <m1raz3nt@gmail.com>
2018-08-23 11:48:34 +0200

    Minor changes

---
+++ HRTool/DAL/Models/Duty.cs
@@ -0,0 +1,11 @@
+using System;
+
+namespace HRTool.DAL.Models
+{
+    public class Duty
+    {
+        //Коммент для проверки мерджа
+        public Guid DutyId {get;set;}
+        public string DutyName {get;set;}
+    }
+}
